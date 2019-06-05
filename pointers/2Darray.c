60a7845303481e07ca959ef45282a691c95caa74
Madhav Khosla <madhav.khoslaa@gmail.com>
2019-06-05 10:07:41 +0530

    idk

--- pointers/2Darray.c
+++ pointers/2Darray.c
@@ -1,18 +1,22 @@
 #include <stdio.h>
+const int M =3;
+const int N =3;
 
-void main(){
-	int a[3][3];
-	int* p[3];
-	for(int i=0; i<3; i++){
-		for(int j=0; j<3; j++){
-			a[i][j] = rand();
-		}
-	}
-	for(int k=0; k<3; k++){
-		for(int l=0 ; l<3; l++){
+void printma(int a[M][N]){
+	for(int k=0; k<M; k++){
+		for(int l=0 ; l<N; l++){
 			printf("%d ", a[k][l]);
-				}
+		}
 		printf("\n");
 		}
-	}
+}
+
+void main(){
+	int a[3][3] = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+	printma(&a);
+	int* p;
+	p=a;
+	printf("%d a[2][3]", *((a[2])+3));
+
+}
 
