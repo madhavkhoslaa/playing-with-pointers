ea4407e7c55b18a6329f5ac90d72d44dd77b1245
Madhav Khosla <madhav.khoslaa@gmail.com>
2019-06-05 07:32:48 +0530

    godspeeeeeed2

--- pointers/2Darray.c
+++ pointers/2Darray.c
@@ -1,7 +1,18 @@
 #include <stdio.h>
 
 void main(){
-	int a[5][5];
-	int* p[5];
-	p= a;
-}
+	int a[3][3];
+	int* p[3];
+	for(int i=0; i<3; i++){
+		for(int j=0; j<3; j++){
+			a[i][j] = rand();
+		}
+	}
+	for(int k=0; k<3; k++){
+		for(int l=0 ; l<3; l++){
+			printf("%d ", a[k][l]);
+				}
+		printf("\n");
+		}
+	}
+
